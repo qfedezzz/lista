095746ba9550f148dd5da2d1abf4a8204e84e8f4
Quim <157989268+qfedezzz@users.noreply.github.com>
2025-03-05 21:42:18 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// Importar Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, updateDoc, deleteDoc, doc, query, orderBy } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
+// Configuración de Firebase (REEMPLAZA ESTO CON TU CONFIGURACIÓN)
+const firebaseConfig = {
+    apiKey: "AIzaSyCHFzGTHNrZKVqpI7wqjOTWCpG_llqonMY",
+    authDomain: "lista-12899.firebaseapp.com",
+    projectId: "lista-12899",
+    storageBucket: "lista-12899.firebasestorage.app",
+    messagingSenderId: "841578650890",
+    appId: "1:841578650890:web:6d0dd9f655eb230994eef3"
+  };
+
+// Inicializar Firebase y Firestore
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc, getDocs, updateDoc, deleteDoc, doc, query, orderBy };
